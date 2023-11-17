ff2b40fe65a40e3f04ab8eb2999d0ce4e16073aa
vishnuarjun512 <vishnuarjun512@gmail.com>
2023-11-17 11:40:05 +0530

    Listing Create Test

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -3,6 +3,7 @@ import Listing from "../models/listing.model.js";
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
+    console.log("User Created Successfully");
     return res.status(201).json(listing);
   } catch (error) {
     next(error);
