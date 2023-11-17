4d158389616ed48c18fbbbc4ca6ab7a2da2342ab
vishnuarjun512 <vishnuarjun512@gmail.com>
2023-11-17 11:38:58 +0530

    Create Listing Api Route Done successfully

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
 
@@ -38,3 +39,4 @@ app.use((err, req, res, next) => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
