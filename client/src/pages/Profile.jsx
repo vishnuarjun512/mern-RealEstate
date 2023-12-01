928ddffd66bc87279895a5e36ce6fe6418272564
vishnuarjun512 <vishnuarjun512@gmail.com>
2023-12-01 21:34:32 +0530

    Firebase Import Error check

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,7 +7,7 @@ import {
   ref,
   uploadBytesResumable,
 } from "firebase/storage";
-import { app } from "../firebase";
+import { app } from "../Firebase.js";
 import {
   updateUserStart,
   updateUserSuccess,
