370beb8ed457c26f1227b0b76e5aa6a773f615d5
vishnuarjun512 <vishnuarjun512@gmail.com>
2023-12-01 21:26:18 +0530

    Added Preinstallation for Hosting

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 mongoose
@@ -16,6 +17,8 @@ mongoose
     console.log("Couldnt not connect to MongoDB -> " + err);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -24,6 +27,15 @@ app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on Port:3000!!!");
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 //MiddleWare
@@ -36,7 +48,3 @@ app.use((err, req, res, next) => {
     message,
   });
 });
-
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/listing", listingRouter);
